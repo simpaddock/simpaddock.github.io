24a4b34a150dc689b8c115884ba7540e43075e0c
cmllr <fury224@googlemail.com>
2018-09-08 23:38:42 +0200

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: "simpaddock",
+  description: "Open Source Simracing League Toolkit",
+  base: "./",
+  themeConfig: {
+    logo: 'https://avatars1.githubusercontent.com/u/43098224?s=200&v=4',
+    nav: [
+      { text: "GitHub", link: "https://github.com/simpaddock"}
+    ],
+    sidebar: [
+      "/",
+    ],
+    lastUpdated: "Last Updated",
+  }
+}
